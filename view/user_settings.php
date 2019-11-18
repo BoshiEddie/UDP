659ca42b0e5b012f5e18a7a9b7ca2f94c0eddd97
Wenyu Shi <d00198805@student.dkit.ie>
2019-11-18 21:05:00 +0000

    setting page finished

--- view/user_settings.php
+++ view/user_settings.php
@@ -4,99 +4,93 @@ include 'asset/include/header.php';
 session_start();
 $client_information = $_SESSION['user_inform'][0];
 ?>
-<style type="text/css">
-    .nav-img{
-        background-color: #E7EBFD;
-        width: 100%;
-        height: 20%;
-    }
-    .nav-img img{
-        width: 80%;
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
 
-    }
-    .navbar_bottom {
-        overflow: hidden;
-        background-color: #E7EBFD;
-        position: fixed;
-        bottom: 0;
-        width: 100%;
-    }
-
-    .navbar_bottom a {
-        float: left;
-        display: block;
-        color: #4d4f5c;
-        text-align: center;
-        padding: 14px 16px;
-        text-decoration: none;
-        font-size: 17px;
-        width: 33.33333333333%;
-    }
-
-    .navbar_bottom a:hover {
-        background: #f1f1f1;
-        color: black;
-    }
-
-    .navbar_bottom a.active {
-        background-color: #707070;
-        color: white;
-    }
-
-</style>
 <body>
-<div class="nav-img">
-    <img src="asset/images/logo_name_fix.png">
-</div>
-
-<nav class="navbar_bottom">
-    <a href="index.php?action=home">Home</a>
-    <a href="index.php?action=home">Process</a>
-    <a href="index.php?action=setting" class="active">Setting</a>
-</nav>
-
-
-
-<div>
-    <div class="">
-
+<header class="fixed-top">
+    <div class="nav-img ">
+        <img src="asset/images/logo_name_fix.png">
     </div>
-    <div>
-        <h2><?php echo $client_information['firstname'], " ", $client_information['lastname']; ?></h2>
+    <nav class="navbar_bottom fixed-bottom">
+        <a href="index.php?action=home">Home</a>
+        <a href="index.php?action=home">Process</a>
+        <a href="index.php?action=setting" class="active">Setting</a>
+    </nav>
+</header>
 
-        <table border="1" >
-            <thead>
-            <tr>My Info </tr>
-            </thead>
+
+<div class="container cont_overflow">
+    <!-- user profile   -->
+    <div class="user_profile mx-auto border-bottom">
+        <div class="profile_img">
+            <img src="asset/images/JayBrady1.jpg">
+        </div>
+        <div class="profile_details">
+            <p class="font-weight-bolder">
+                <?php echo $client_information['firstname'], " ", $client_information['lastname']; ?>
+            </p>
+            <p class="font-weight-lighter">
+                <?php echo $client_information['address'] ?>
+            </p>
+            <p class="font-weight-lighter">
+                <?php echo $client_information['phone_number'] ?>
+            </p>
+        </div>
+    </div>
+    <!-- user data   -->
+    <div class="user_data mx-auto">
+        <h5>My Info</h5>
+        <table class="table table-bordered table-hover">
             <tbody>
             <tr>
-                <td>Age: </td>
+                <td>Age:</td>
                 <td id="age"><b></b></td>
             </tr>
             <tr>
-                <td>Weight: </td>
+                <td>Weight:</td>
                 <td id="weight"></td>
             </tr>
             <tr>
-                <td>Height: </td>
+                <td>Height:</td>
                 <td id="height"></td>
             </tr>
             <tr>
-                <td>Medical issues: </td>
-                <td id ="medical_issues"></td>
+                <td>Medical issues:</td>
+                <td id="medical_issues"></td>
             </tr>
             </tbody>
         </table>
+    </div>
+    <!-- user's trainer-->
+    <div class="trainer_profile mx-auto border-bottom">
+        <h5>My Trainer</h5>
+        <div class="profile_img">
+            <img src="asset/images/KenColeman.jpg">
+        </div>
+        <div class="profile_details">
+            <p class="font-weight-bolder">
+                Chris Jericho
+            </p>
+            <p class="font-weight-lighter">
+                Dundalk
+            </p>
+            <p class="font-weight-lighter">
+                8715674877
+            </p>
+        </div>
+    </div>
 
-
-        <button>
+    <br><br><br>
+    <div>
+        <button class="butttons">
             <a href="model/logout.php"><i aria-hidden="true"></i> &nbsp;Sign Out</a>
+        </button>
+        <button class="butttons">
+            <a href=""><i aria-hidden="true"></i> &nbsp;Reset Password</a>
         </button>
 
     </div>
+
+</div>
 </body>
 <?php include 'asset/include/footer.php'; ?>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
@@ -110,8 +104,8 @@ $client_information = $_SESSION['user_inform'][0];
         let date = new Date();
         let startDate = new Date(birthday);
         let newDate = date.getTime() - startDate.getTime();
-        let age = Math.ceil(newDate / 1000 / 60 / 60 / 24 /365);
-        if (isNaN(age)){
+        let age = Math.ceil(newDate / 1000 / 60 / 60 / 24 / 365);
+        if (isNaN(age)) {
             age = "";
         }
         $("#age").html(age);
